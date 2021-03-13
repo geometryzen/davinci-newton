0567934bf7434c424c646dd583341521dd16a2f1
David Geo Holmes <geometryzen@gmail.com>
2021-03-13 17:35:13 -0500

    Rod2

--- build/browser/index.d.ts
+++ build/browser/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for davinci-newton 1.0.37
+// Type definitions for davinci-newton 1.0.38
 // Project: https://github.com/geometryzen/davinci-newton
 // Definitions by: David Geo Holmes david.geo.holmes@gmail.com https://www.stemcstudio.com
 //
