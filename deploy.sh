a6865c707820f0ef456d2599cf3e9b607e0dcb90
David Geo Holmes <geometryzen@gmail.com>
2024-04-03 06:53:40 -0400

    'Evergreen'

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-echo "Please enter a commit message"
-read message
-echo "Please enter tag version. e.g. major.minor.patch"
-read version
 npm install
 npm update
 npm run build
-npm run docs
-npm run pages
-npm run build
+npm run lint
+npm run test
+npm run check
 git status
 git add --all
-git commit -m "$message"
-git tag -a "$version" -m "$message"
-git push origin master --tags
-npm publish
+echo "Please enter a commit message"
+read message
+git commit -m "'$message'"
+git push origin main
+npm run release
+npm run docs
+npm run pages
