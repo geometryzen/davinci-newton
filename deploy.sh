40896f04902a28ac12369515914b3f9be93d9917
David Geo Holmes <geometryzen@gmail.com>
2024-04-03 06:54:49 -0400

    'Evergreen'

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ git add --all
 echo "Please enter a commit message"
 read message
 git commit -m "'$message'"
-git push origin main
+git push origin master
 npm run release
 npm run docs
 npm run pages
